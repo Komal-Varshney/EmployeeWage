d92fb319e1c19a9734c5b4ca2446c21195909607
Komal-Varshney <varshneyk578@gmail.com>
2024-12-31 16:46:51 +0530

    Added : Part time wage

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Employee e1 = new Employee("Komal", 125);
         Random r = new Random();
-        int i = (r.nextInt(1000))%2;
+        int i = (r.nextInt(1000))%3;
         switch(i){
             case 1 -> e1.attendance(true);
+            case 2 ->
+            {
+                e1.attendance(true);
+                System.out.println("Part time");
+                e1.normalWorkday = 4;
+            }
             default -> e1.attendance(false);
         }
         e1.calculateWage();
